e50ab8adcd24d5915ba0261f96f034c4ecb660a6
KING YONG <won01288@gmail.com>
2026-07-14 21:52:55 +0900

    config_Apps Script 웹앱 URL 입력

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Google Apps Script 웹앱 배포 후 발급되는 URL을 여기에 붙여넣으세요.
 // 예: https://script.google.com/macros/s/AKfycb.../exec
 // (apps-script/Code.gs 를 Apps Script 프로젝트에 붙여넣고 "웹앱으로 배포"한 뒤 나오는 주소입니다)
-const API_URL = "여기에_APPS_SCRIPT_웹앱_URL을_붙여넣으세요";
+const API_URL = "https://script.google.com/macros/s/AKfycbxqCHEjHMlekqZcXIEmUAS-C10SMDyyZf1YLk3xmcKkpTXWljRPkqXzHYg7KbqfqKY/exec";
